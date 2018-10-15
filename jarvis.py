28a57c96b68d148bbf0198503b5acf9275c5360a
Philamer Sune <pvsune@hotmail.com>
2018-10-15 15:06:59 +0800

    Init jarvis deployment using DialogDelegate.

--- jarvis.py
+++ jarvis.py
@@ -1,30 +1,52 @@
 import json
 
 import falcon
-from falcon_ask import dispatch_request, FalconAskMiddleware, respond
+from falcon_ask import dispatch_request, FalconAskMiddleware, respond, util
 
 
-def intent_fn(body):
-    # "body" contains request POST data.
-    return 'Congratulations! Your new alexa skill works great.'
+def deploy_fn(body):
+    response = None
+    end_session = False
+    directives = [{'type': 'Dialog.Delegate'}]
+
+    req = body.get('request', {})
+    dialog_state = req.get('dialogState')
+    intent_status = req.get('intent', {}).get('confirmationStatus')
+
+    if dialog_state == 'COMPLETED':
+        directives = []
+        end_session = True
+        response = 'OK! You can ask me to deploy your app again sometime.'
+        if intent_status == 'CONFIRMED':
+            response = "Yeehaw! You're app is now being deployed."
+
+    return respond(response, end_session=end_session, directives=directives)
+
+
+def unknown_fn(body):
+    return respond(
+        "You've found something I don't know yet. Try asking again sometime.",
+        end_session=True
+    )
 
 
 class JarvisResource(object):
-    # Dictionary mapping of "IntentRequest" to function.
     intent_maps = {
-        'GreetingIntent': intent_fn,
+        'DeployIntent': deploy_fn,
+        'UnknownIntent': unknown_fn,
     }
 
-    # Message to return when "LaunchRequest" is received.
-    welcome = 'Hi, welcome to your new alexa skill.'
+    welcome = respond(
+        "Howdy! I'm Jarvis, I can help you deploy your app.",
+        end_session=False,
+    )
 
     def on_post(self, req, resp):
         response = dispatch_request(req)
-        resp.body = json.dumps(respond(response, end_session=False))
+        resp.body = json.dumps(response)
 
 
 app = falcon.API(middleware=[
-    # Do validation of request certificate and timestamp.
     FalconAskMiddleware(JarvisResource, validate=True),
 ])
 app.add_route('/', JarvisResource())
